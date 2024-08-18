3de2debac20b907078f624d7a9aa8a1b5c0cf582
Chanchal Choudhary <chanchal.choudhary@fcsamerica.com>
2024-08-18 18:54:42 -0500

    versioning with route

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -10,7 +10,7 @@ namespace CityInfo.API.Controllers
 {
     [ApiController]
     //[Authorize]  [Authorize]
-    [Route("api/cities")]
+    [Route("api/v{version:apiVersion}/cities")]
     [ApiVersion(1)]
     [ApiVersion(2)] // one can have as many as versions
     public class CitiesController : ControllerBase
--- CityInfo/CityInfo.API/Controllers/FilesController.cs
+++ CityInfo/CityInfo.API/Controllers/FilesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.StaticFiles;
 
 namespace CityInfo.API.Controllers
 {
-    [Route("api/files")]
+    [Route("api/v{version:apiVersion}/files")]
     [Authorize]
     [ApiController]
     public class FilesController : ControllerBase
